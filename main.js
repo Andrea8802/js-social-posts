e291e6044f824b180e8b0f93c77f75a59e95356a
Andrea8802 <andreabonaventura802@gmail.com>
2022-11-23 16:04:18 +0100

    header and body post done

---
+++ main.js
@@ -0,0 +1,87 @@
+const posts = [
+    {
+        "id": 1,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=171",
+        "author": {
+            "name": "Phil Mangione",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        "likes": 80,
+        "created": "2021-06-25"
+    },
+    {
+        "id": 2,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=112",
+        "author": {
+            "name": "Sofia Perlari",
+            "image": "https://unsplash.it/300/300?image=10"
+        },
+        "likes": 120,
+        "created": "2021-09-03"
+    },
+    {
+        "id": 3,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=234",
+        "author": {
+            "name": "Chiara Passaro",
+            "image": "https://unsplash.it/300/300?image=20"
+        },
+        "likes": 78,
+        "created": "2021-05-15"
+    },
+    {
+        "id": 4,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=24",
+        "author": {
+            "name": "Luca Formicola",
+            "image": null
+        },
+        "likes": 56,
+        "created": "2021-04-03"
+    },
+    {
+        "id": 5,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=534",
+        "author": {
+            "name": "Alessandro Sainato",
+            "image": "https://unsplash.it/300/300?image=29"
+        },
+        "likes": 95,
+        "created": "2021-03-05"
+    }
+];
+
+const container = document.getElementById("container");
+
+
+
+// Creare array di oggetti per i post
+posts.forEach((element, i) => {
+    container.innerHTML = `<div class="post"></div>`;
+    
+    const post = document.querySelector(".post");
+    post.innerHTML += `<div class="post__header"></div>`;
+
+    const postHeader = document.querySelector(".post__header");
+    postHeader.innerHTML = `<div class="post-meta"></div>`;
+
+    const postMeta = document.querySelector(".post-meta");
+    postMeta.innerHTML += `<div class="post-meta__icon"> <img class="profile-pic" src="${element.author.image}" alt="${element.author.name}"> </div>`;
+    
+    postMeta.innerHTML += `<div class="post-meta__data"></div>`;
+    
+    const postMetaData = document.querySelector(".post-meta__data");
+    postMetaData.innerHTML += `<div class="post-meta__author">${element.author.name}</div>`;
+    postMetaData.innerHTML += `<div class="post-meta__time">${element.created}</div>`;
+
+    post.innerHTML += `<div class="post__text">${element.content}</div>`
+
+    post.innerHTML += `<div class="post__image"> <img src="${element.media}"> </div>`;
+
+    
+});
